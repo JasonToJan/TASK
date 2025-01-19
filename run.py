85d7924ae647bf60ef6828672bafc3c508b6e9e5
JasonJan <jason.wang@ishuyun.com>
2025-01-19 15:19:59 +0800

    hello world 成功了

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from app import create_app
+from app import create_app, db
 
 # 打印当前的 Python 路径
 import sys
@@ -15,4 +15,4 @@ def index():
     return 'Hello, Flask!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
